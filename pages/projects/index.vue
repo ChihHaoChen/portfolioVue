7716d644a2b7e09fa918b08436f535f6b2a4f7d7
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2019-08-06 18:41:23 -0400

    Modify the v-card and components' layouts.

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -6,7 +6,39 @@
         <li  v-for="schedule in schedules" class="StepProgress-item" v-bind:class="'is-done'">
           <div class="bold time">{{`${schedule.start} - ${schedule.end}`}} </div> 
           <div style="margin: 0 0 0 68px;" class="bold">{{schedule.title}}</div>
-          <div style="margin: 0 0 0 68px">{{schedule.description}}</div>
+          <!-- <div style="margin: 0 0 0 68px">{{schedule.description}}</div> -->
+          <v-container
+            class="pa-2"
+            fluid
+            grid-list-md
+          >
+          <v-layout class="card-layout" column>
+            <v-flex xs12>
+              <v-card flat class="card-item" width=640 :color="schedule.color" column>
+                <v-flex xs6>
+                  <v-list-item three-line>
+                    <v-list-item-content class="align-self-start">
+                      <v-list-item-title
+                        class="headline mb-2"
+                        v-text="schedule.title"
+                      ></v-list-item-title>
+
+                      <v-list-item-subtitle v-text="schedule.artist"></v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-flex>   
+                <v-flex xs6>  
+                  <v-list-item-avatar
+                    size="125"
+                    tile
+                  >
+                    <v-img :src="schedule.src"></v-img>
+                  </v-list-item-avatar>
+                </v-flex>
+              </v-card>
+            </v-flex>
+          </v-layout>
+          </v-container>
         </li>
       </ul>
       </div>
@@ -24,13 +56,22 @@ export default {
         start: "2017",
         end: "2019",
         title: "test",
-        description: "This is a test"
+        description: "This is a test",
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People'
+        
       },
        {
         start: "2019",
         end: "2020",
         title: "test2",
-        description: "This is a test2"
+        description: "This is a test2",
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Days',
+        artist: 'Ellie Goulding'
       },
       
       ],
@@ -39,7 +80,21 @@ export default {
         end: "2019",
       title: "test",
       t: '1em'
-      }
+      },
+      items: [
+        {
+          color: '#1F7087',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+        },
+        {
+          color: '#952175',
+          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+          title: 'Halcyon Days',
+          artist: 'Ellie Goulding',
+        },
+      ],
     }
 
   },
@@ -54,9 +109,9 @@ export default {
 .time{position:absolute; left:-115px;}
 .timeline-wrapper {
   padding-left:80px;
-  min-width: 400px;
+  min-width: 800px;
   font-family: 'Helvetica';
-  font-size: 14px;
+  font-size: 20px;
   /*border: 1px solid #CCC;*/
 }
 .StepProgress {
@@ -67,29 +122,17 @@ export default {
   -webkit-transition: 2s; /* For Safari 3.1 to 6.0 */
   transition: 2s;
 }
-.StepProgress::before {
-  display: inline-block;
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 85px;
-  width: 52px;
-  height: 100%;
-  border-left: 2px solid #CCC;
-}
 .StepProgress-item {
   
   position: relative;
   counter-increment: list;
 }
-.StepProgress-item:not(:last-child) {
-  padding-bottom: 20px;
-}
+
 .StepProgress-item::before {
   display: inline-block;
   content: '';
   position: absolute;
-  left: 40px;
+  left: 38px;
   height: 100%;
   width: 10px;
 }
@@ -107,7 +150,6 @@ export default {
 }
 .StepProgress-item.is-done::before {
   border-left: 2px solid green;
-
 }
 .StepProgress-item.is-done::after {
   /*content: "?";*/
@@ -119,6 +161,12 @@ export default {
 }
 .StepProgress strong {
   display: block;
+}
+.card-layout  {
+  padding-left: 50px;
+}
+.card-item  {
+  font-size: 20px;
 }
 </style>
 
