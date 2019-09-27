b9aa0a29e6d6871a649b19f9d2cac5e466755c23
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2019-09-26 23:24:26 -0400

    Temporarily add the background image.

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>
-    Test
-  </div>
+  <v-layout fluid column wrap>
+    <v-flex xs12 sm12>
+      <v-parallax class = "homepage" :src="require('@/assets/images/background.jpg')">
+      </v-parallax>
+    </v-flex>
+  </v-layout>
+  
+
 </template>
 
 <script>
 export default {
+
 }
 </script>
 
 <style scoped>
+  .homepage {
+    transform: none;
+    width: 100%;
+  }
 </style>
 
 
